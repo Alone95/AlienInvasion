ef49e9e572cee9a3dee77fc0b5ded1a28f640786
Alone95 <460454903@qq.com>
2017-05-03 22:31:41 +0800

    first commit

---
+++ alien_invasion.py
@@ -0,0 +1,37 @@
+import pygame
+from  settings import Settings
+from ship import Ship
+import game_functions as gf
+
+def run_game():
+    #初始化pygame,设置和屏幕对象
+    
+    pygame.init() #初始化背景设置
+    ai_settings =Settings()
+    screen = pygame.display.set_mode(
+        (ai_settings.screen_width,ai_settings.screen_height))
+    pygame.display.set_caption("Alien Invasion")
+
+    
+
+    #设置背景色
+    bg_color =(230,230,230)
+
+
+    #创建一艘飞船
+    ship =Ship(ai_settings,screen)
+    
+
+    #开始游戏的主循环
+    while True:
+
+
+        #监视键盘和鼠标事件
+        gf.check_events(ship)
+        ship.update()
+        #每次循环都重绘屏幕
+        gf.update_screen(ai_settings,screen,ship)
+        
+
+
+run_game()
