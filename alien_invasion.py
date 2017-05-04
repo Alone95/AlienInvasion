cb1d9a0cc90535efe3086f87c6949bd6195774d1
Alone95 <460454903@qq.com>
2017-05-04 16:42:35 +0800

    second commit

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,7 @@ import pygame
 from  settings import Settings
 from ship import Ship
 import game_functions as gf
+from pygame.sprite import Group
 
 def run_game():
     #初始化pygame,设置和屏幕对象
@@ -20,6 +21,9 @@ def run_game():
 
     #创建一艘飞船
     ship =Ship(ai_settings,screen)
+
+    #创建一个用于存储子弹的编组
+    bullets = Group()
     
 
     #开始游戏的主循环
@@ -27,10 +31,13 @@ def run_game():
 
 
         #监视键盘和鼠标事件
-        gf.check_events(ship)
+        gf.check_events(ai_settings,screen,ship,bullets)
         ship.update()
+        gf.update_bullets(bullets)
+        
+        
         #每次循环都重绘屏幕
-        gf.update_screen(ai_settings,screen,ship)
+        gf.update_screen(ai_settings,screen,ship,bullets)
         
 
 
