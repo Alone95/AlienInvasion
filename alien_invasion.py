89e73f8875ae14d9af59c7230fc0fea3e438f77a
Alone95 <460454903@qq.com>
2017-05-06 12:44:22 +0800

    aliens can move

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,7 @@ from  settings import Settings
 from ship import Ship
 import game_functions as gf
 from pygame.sprite import Group
+from alien import Alien
 
 def run_game():
     #初始化pygame,设置和屏幕对象
@@ -19,12 +20,15 @@ def run_game():
     bg_color =(230,230,230)
 
 
-    #创建一艘飞船
-    ship =Ship(ai_settings,screen)
-
-    #创建一个用于存储子弹的编组
+    #创建一艘飞船,一个子弹编组和一个外星人编组
+    ship = Ship(ai_settings,screen)
     bullets = Group()
+    aliens = Group()
+
     
+    #创建外星人群
+    #alien = Alien(ai_settings, screen)
+    gf.create_fleet(ai_settings, screen, ship, aliens)
 
     #开始游戏的主循环
     while True:
@@ -34,10 +38,14 @@ def run_game():
         gf.check_events(ai_settings,screen,ship,bullets)
         ship.update()
         gf.update_bullets(bullets)
-        
-        
+        gf.update_aliens(ai_settings,aliens)
         #每次循环都重绘屏幕
-        gf.update_screen(ai_settings,screen,ship,bullets)
+        gf.update_screen(ai_settings,screen,ship,aliens,bullets)
+        
+        
+
+
+
         
 
 
--- settings.py
+++ settings.py
@@ -4,9 +4,9 @@ class Settings():
     def __init__(self):
         """ 初始化游戏的设置"""
         #屏幕设置
-        self.screen_width=800
-        self.screen_height=600
-        self.bg_color =(230,230,230)
+        self.screen_width = 1024
+        self.screen_height = 600
+        self.bg_color = (230,230,230)
         self.ship_speed_factor = 1.5
 
         #子弹设置
@@ -15,4 +15,11 @@ class Settings():
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
         self.bullets_allowed = 3
-        
+
+        #外星人设置
+        self.alien_speed_factor = 1
+        self.fleet_drop_speed = 10
+        #fleet_direction为1表示向右移 为-1表示向左移
+        self.fleet_direction = 1
+
+
